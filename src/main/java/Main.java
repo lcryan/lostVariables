f206d0ae19a2e53527ad27a195aaf33c04b9eebe
LuciaRyan <lcryan06@gmail.com>
2023-05-06 23:18:20 +0200

    ran tests / debug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         String string_voorbeeld = "Dit is een voorbeeld";
-        String string_hello_world = "Hello World!";
+        String string_hello_world = "hello world";
         boolean boolean_true = true;
         int int_four = 4;
         short short_minus_eight = -8;
         float float_six_point_five = 6.5f;
-        double double_minus_two_point_three = 2.3;
-        boolean_true = false;
-
-        float_six_point_five = 6.25f;
+        double double_minus_two_point_three = 2.3d;
 
 
         // Wijzig niets aan onderstaande System.out.println statements
@@ -25,7 +22,7 @@ public class Main {
         System.out.println(double_minus_two_point_three); // double -2.3
 
         // Bonus: Wijs een nieuwe waarde toe aan een bestaande variabele
-
+        string_voorbeeld = "Dit is een aangepast voorbeeld";
         System.out.println(string_voorbeeld); // String Dit is een aangepast voorbeeld
     }
 
